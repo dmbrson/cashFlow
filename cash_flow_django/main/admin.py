8035a456a8f1c35ab4b57a18f34c9dd769fa5fbb
Danil <dka04@list.ru>
2025-01-13 21:23:53 +1000

    edit admin, record_list.html, views.py

--- cash_flow_django/main/admin.py
+++ cash_flow_django/main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Status, Type, Category, SubCategory, CashFlowRecord
 
-# Register your models here.
+admin.site.register(Status)
+admin.site.register(Type)
+admin.site.register(Category)
+admin.site.register(SubCategory)
+admin.site.register(CashFlowRecord)
--- cash_flow_django/main/views.py
+++ cash_flow_django/main/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 
-def record_list(request):
-    return render(request, 'main/record_list.html', {
+from main.models import CashFlowRecord
 
+
+#Главная страница: список записей
+def record_list(request):
+    records = CashFlowRecord.objects.all()
+    return render(request, 'main/record_list.html', {
+        'records': records,
     })
